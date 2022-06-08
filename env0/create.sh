30468292db5b45640d538a8aa60385183a7de3c0
khoffska <45382758+khoffska@users.noreply.github.com>
2022-06-08 13:43:26 -0400

    asdf

--- env0/create.sh
+++ env0/create.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 externalid="env0"$(date +%d%m)
+sed -i "s/externalid/$externalid/" trust.json
 aws iam create-role --role-name Cloudnexa-Env0 --assume-role-policy-document file://trust.json
 aws iam update-role --role-name Cloudnexa-Env0 --description 'allows Env0 access Cloudnexa' --max-session-duration 28800
 aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/CloudWatchFullAccess --role-name Cloudnexa-Env0
