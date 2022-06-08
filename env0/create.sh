1aa0edcb614240213ffa79fefc7b4b73e0b41eb1
khoffska <45382758+khoffska@users.noreply.github.com>
2022-06-08 13:53:19 -0400

    Update create.sh

--- env0/create.sh
+++ env0/create.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 externalid="env0"$(date +%d%m)
+snstopic=$(aws sns list-topics | grep cloudwatch)
 sed -i "s/externalid/$externalid/" trust.json
 aws iam create-role --role-name Cloudnexa-Env0 --assume-role-policy-document file://trust.json
 aws iam update-role --role-name Cloudnexa-Env0 --description 'allows Env0 access Cloudnexa' --max-session-duration 28800
@@ -7,3 +8,5 @@ aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/CloudWatchFullAc
 aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/ReadOnlyAccess --role-name Cloudnexa-Env0
 aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess --role-name Cloudnexa-Env0
 aws iam get-role --role-name Cloudnexa-Env0
+rolearn=$(aws iam get-role --role-name Cloudnexa-Env0 | grep Arn)
+echo "ExternalID is $externalid Sns topic is $snstopic RoleARN is $rolearn "
