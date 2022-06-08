0d475844561174c89932d00760255c471fe12767
khoffska <45382758+khoffska@users.noreply.github.com>
2022-06-08 13:05:09 -0400

    creatrole

---
+++ env0/create.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+aws iam create-role --role-name Cloudnexa-Env0 --assume-role-policy-document file://trust.json
+aws iam update-role --role-name Cloudnexa-Env0 --description 'allows Env0 access Cloudnexa' --max-session-duration 28800
+aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/CloudWatchFullAccess --role-name Cloudnexa-Env0
+aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/ReadOnlyAccess --role-name Cloudnexa-Env0
+aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess --role-name Cloudnexa-Env0
+aws iam get-role --role-name Cloudnexa-Env0
